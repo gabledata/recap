99f2336f215cc0d0ce5a60e423250c77480d5467
Chris Riccomini <criccomini@apache.org>
2022-12-31 14:03:19 -0800

    Add `recap plugins` command
    Add a simple `recap plugins` command that lists the current plugins. The
    command has four subcommands: `analyzers`, `browsers`, `catalogs`, and
    `commands`. Specifying one will list the plugins for that type.
    
    ```
    recap plugins analyzers
    [
      "db.access",
      "db.column",
      "db.comment",
      "db.foreign_key",
      "db.index",
      "db.location",
      "db.primary_key",
      "db.profile",
      "db.view_definitions"
    ]
    ```

--- recap/cli.py
+++ recap/cli.py
@@ -1,10 +1,10 @@
 import typer
 from .logging import setup_logging
-from .plugins import load_command_plugins
+from .plugins import init_command_plugins
 
 
 LOGGING_CONFIG = setup_logging()
-app = load_command_plugins(typer.Typer())
+app = init_command_plugins(typer.Typer())
 
 
 if __name__ == "__main__":
