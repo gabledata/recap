adb2f5cd566bb51be8467f9f91478e3ceea2ea7d
Chris Riccomini <criccomini@apache.org>
2023-01-24 10:59:20 -0800

    Replace `analyze` and `browse` commands with `live` commands
    The `recap analyze` and `recap browse` commands felt a little clunky to me.
    I've replaced them with `recap live read` and `recap live list` (similar to
    their `recap catalog` counterparts). Docs have been updated, too.

--- recap/commands/live.py
+++ recap/commands/live.py
@@ -1,16 +1,21 @@
+import logging
 import typer
+from pathlib import PurePosixPath
 from recap.analyzers import create_analyzer
+from recap.browsers import create_browser
 from recap.config import settings
 from recap.paths import create_catalog_path
+from recap.plugins import load_browser_plugins
 from recap.typing import AnalyzerInspector
 from rich import print_json
 
 
-app = typer.Typer()
+app = typer.Typer(help="Read live metadata from a system, bypassing the catalog.")
+log = logging.getLogger(__name__)
 
 
 @app.command()
-def analyze(
+def read(
     plugin: str = typer.Argument(...,
         help="Analyzer plugin name. Run `recap plugins analyzers` to get a full list.",
     ),
@@ -22,7 +27,7 @@ def analyze(
     ),
 ):
     """
-    Analyzes and returns metadata for a URL and path.
+    Analyzes and prints metadata for a URL and path.
     """
 
     crawlers_configs = settings('crawlers', [])
@@ -58,3 +63,46 @@ def analyze(
                     raise ValueError(
                         f"Invalid path={path} for analyzer={plugin}",
                     )
+
+
+@app.command("list")
+def list_(
+    url: str = typer.Argument(...,
+        help="URL to browse. URL configs in settings.toml will be used, if availabe.",
+    ),
+    path: str = typer.Argument(
+        '/',
+        help="Path to list children for.",
+    ),
+):
+    """
+    Lists directory children for a URL and path.
+    """
+
+    clean_path = str(PurePosixPath(path))
+    browser_plugins = load_browser_plugins()
+    crawlers_configs = settings('crawlers', [])
+    crawler_urls = set(map(
+        lambda c: c.get('url'),
+        crawlers_configs,
+    ))
+
+    if url and url not in crawler_urls:
+        crawlers_configs.append({'url': url})
+
+    for crawler_config in crawlers_configs:
+        if url == crawler_config['url']:
+            for browser_name in browser_plugins.keys():
+                try:
+                    with create_browser(browser_name, **crawler_config) as browser:
+                        if children := browser.children(clean_path):
+                            print_json(data=sorted([c.name() for c in children]))
+                        else:
+                            print_json(data=[])
+                except Exception as e:
+                        log.debug(
+                            'Skipped browser for url=%s name=%s',
+                            url,
+                            browser_name,
+                            exc_info=e,
+                        )
