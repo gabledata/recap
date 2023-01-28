69ca1beeb6e51de49317bcaa4d7206ea3db97cec
Nahum Rosa Cruz Sa <49600259+nahumsa@users.noreply.github.com>
2023-01-28 03:52:36 +0000

    add ls test

--- tests/catalog/test_db.py
+++ tests/catalog/test_db.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from recap.catalogs.db import DatabaseCatalog, CatalogEntry
 
 class TestCatalogEntry:
-    def test_catalog_entry_is_deleted(self):
+    def test_is_deleted(self):
         entry = CatalogEntry(deleted_at=datetime.now())
         assert entry.is_deleted() == True
 
@@ -14,33 +14,46 @@ class TestCatalogEntry:
         assert entry.is_deleted() == False
 
 class TestDatabaseCatalog:
-    def test_database_catalog_init(self):
-        engine = create_engine("sqlite:///:memory:")
-        catalog = DatabaseCatalog(engine)
+    @pytest.fixture
+    def engine(self):
+        return create_engine('sqlite:///:memory:')
+
+    @pytest.fixture
+    def catalog(self, engine):
+        return DatabaseCatalog(engine)
+
+    def test_catalog_init(self, engine, catalog):
         assert isinstance(catalog.Session(), Session)
 
-    def test_database_catalog_touch(self):
+    def test_catalog_touch(self):
         engine = create_engine("sqlite:///:memory:")
         catalog = DatabaseCatalog(engine)
 
         # Test touching a path that doesn't exist
-        catalog.touch("/path/to/file")
+        catalog.touch("/databases/table")
         with catalog.Session() as session:
-            result = session.query(CatalogEntry).filter_by(parent="/path/to", name="file").first()
+            result = session.query(CatalogEntry).filter_by(parent="/databases", name="table").first()
             assert result.metadata_ == {}
 
         # Test touching a path that does exist
-        catalog.touch("/path/to/file")
+        catalog.touch("/databases/table")
         with catalog.Session() as session:
-            result = session.query(CatalogEntry).filter_by(parent="/path/to", name="file").first()
+            result = session.query(CatalogEntry).filter_by(parent="/databases", name="table").first()
             assert result.metadata_ == {}
 
         # Test touching a path that has been deleted
         with catalog.Session() as session:
-            entry = session.query(CatalogEntry).filter_by(parent="/path/to", name="file").first()
+            entry = session.query(CatalogEntry).filter_by(parent="/databases", name="table").first()
             entry.deleted_at = datetime.now()
 
-        catalog.touch("/path/to/file")
+        catalog.touch("/databases/table")
         with catalog.Session() as session:
-            result = session.query(CatalogEntry).filter_by(parent="/path/to", name="file").first()
+            result = session.query(CatalogEntry).filter_by(parent="/databases", name="table").first()
             assert result.metadata_ == {}
+
+    def test_ls(self, catalog):
+        catalog.write('/databases/schema/table_one', {})
+        catalog.write('/databases/schema/table_two', {})
+        catalog.write('/databases/schema/table_three', {})
+        assert set(catalog.ls('/databases/schema')) == set(['table_one','table_two','table_three'])
+
