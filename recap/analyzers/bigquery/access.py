f58287fd1bf3892041f33846d56e67d645da008f
Chris Riccomini <criccomini@apache.org>
2023-02-02 09:55:32 -0800

    Add BigQueryAccessAnalyzer for views, tables, and datasets
    Recap now gets BigQuery access information for views, tables, and datasets.
    
    This analyzer is more complex that it should be, but BigQuery's access API is a
    mess. They've just introduced table-level access controls, which go through the
    `get_iam_policy` API rather than the `dataset.access_entries` attribute.
    BigQueryAccessAnalyzer munges these two data structures into a standard-ish
    format called `AccessEntry`. See the docs on the analyzer for more information.
    
    And here's some documentation on Google's AccessEntry class for datasets (which
    is different from Recap's).
    
    https://github.com/googleapis/python-bigquery/blob/bd1da9aa0a40b02b7d5409a0b094d8380e255c91/google/cloud/bigquery/dataset.py#L225
    
    I also explored using `information_schema.object_privileges`, but it seems less
    efficient (and I think it costs $ to use). Here are some docs on that approach:
    
    https://cloud.google.com/bigquery/docs/information-schema-object-privileges
    
    And an example query:
    
    ```
    SELECT *
    FROM `some-project-1234`.`region-us`.INFORMATION_SCHEMA.OBJECT_PRIVILEGES
    WHERE object_name='austin_311'
    ```
    
    And here are some docs on getting (and granting) access to a "resource"
    (dataset, table, or view):
    
    https://cloud.google.com/bigquery/docs/control-access-to-resources-iam#python
    
    NOTE: I didn't bother with routines or external sources for now.

---
+++ recap/analyzers/bigquery/access.py
@@ -0,0 +1,115 @@
+from contextlib import contextmanager
+from google.cloud.bigquery import Client
+from recap.analyzers.abstract import AbstractAnalyzer, BaseMetadataModel
+from recap.browsers.db import SchemaPath, TablePath, ViewPath
+from typing import Generator
+from urllib.parse import urlparse
+
+
+class AccessEntry(BaseMetadataModel):
+    role: str
+    type: str
+    id: str
+
+
+class Access(BaseMetadataModel):
+    __root__: list[AccessEntry] = []
+
+
+class BigQueryAccessAnalyzer(AbstractAnalyzer):
+    """
+    Fetches table, view, and dataset access information from BigQuery.
+
+    BigQuery's Python client and access API are a mess. There are two entirely
+    different endpoints.
+
+    Tables use `get_iam_policy`, which returns a list of bindings like:
+
+        [{"role": "OWNER", members: ["user:user@email.com"]]
+
+    The dataset API is entirely different. The
+    `google.cloud.bigquery.dataset.AccessEntry`s you get are documented
+    [here](https://github.com/googleapis/python-bigquery/blob/bd1da9aa0a40b02b7d5409a0b094d8380e255c91/google/cloud/bigquery/dataset.py#L225).
+
+    BigQueryAccessAnalyzer does its best to map these disjoint structures into
+    a standard `recap.analyzers.bigquery.access.AccessEntry`.
+    """
+
+    def __init__(self, client: Client):
+        self.client = client
+
+    def analyze(
+        self,
+        path: SchemaPath | TablePath | ViewPath,
+    ) -> Access | None:
+        """
+        :param path: Fetch access information for a table, view, or dataset at
+            this path.
+        :returns: A list of `AccessEntry`s containint a role, id, and the type
+            (user, specialGroup, etc) of the id.
+        """
+
+        results = []
+        match path:
+            case TablePath() | ViewPath():
+                name = path.table if isinstance(path, TablePath) else path.view
+                table_id = f"{self.client.project}.{path.schema_}.{name}"
+                policy = self.client.get_iam_policy(table_id)
+                for binding in policy.bindings:
+                    role = binding['role']
+                    for member in binding['members']:
+                        type_, id = member.split(':')
+                        results.append(AccessEntry(
+                            role=role,
+                            type=type_,
+                            id=id,
+                        ))
+            case SchemaPath():
+                dataset = self.client.get_dataset(path.schema_)
+                for entry in dataset.access_entries:
+                    assert (
+                        entry.role
+                        and entry.entity_type
+                        and entry.entity_id
+                    )
+                    id = None
+                    match entry.entity_id:
+                        case str():
+                            id = entry.entity_id
+                        case {'projectId': str(), 'datasetId': str(), 'tableId': str()}:
+                            project_id = entry.entity_id['projectId']
+                            dataset_id = entry.entity_id['datasetId']
+                            table_id = entry.entity_id['tableId']
+                            id = f"`{project_id}`.`{dataset_id}`.`{table_id}`"
+                        case {'projectId': str(), 'datasetId': str(), 'routineId': str()}:
+                            project_id = entry.entity_id['projectId']
+                            dataset_id = entry.entity_id['datasetId']
+                            routine_id = entry.entity_id['routineId']
+                            id = f"`{project_id}`.`{dataset_id}`.`{routine_id}`"
+                        case {'dataset': _, 'target_types': str()}:
+                            project_id = entry.entity_id['dataset']['projectId']
+                            dataset_id = entry.entity_id['dataset']['datasetId']
+                            target_types = entry.entity_id['target_types']
+                            id = f"`{project_id}`.`{dataset_id}`.`{target_types}`"
+                        case _:
+                            raise ValueError(
+                                f"Couldn't parse AccessEntry={entry}"
+                            )
+                    results.append(AccessEntry(
+                        role=entry.role,
+                        type=entry.entity_type,
+                        id=id,
+                    ))
+        return Access.parse_obj(results)
+
+
+@contextmanager
+def create_analyzer(
+    url: str,
+    **_,
+) -> Generator['BigQueryAccessAnalyzer', None, None]:
+    parsed_url = urlparse(url)
+
+    yield BigQueryAccessAnalyzer(Client(
+        project=parsed_url.hostname,
+    ))
