3e5ed809f308cbb88facab52714c5fcfe6a511df
Nahum Rosa Cruz Sa <49600259+nahumsa@users.noreply.github.com>
2023-02-03 18:43:19 -0300

    Create unit tests for `catalog/db.py` and add CI for tests (#149)
    Add unit tests framework and test `DatabaseCatalog`
    
    Recap now has some basic unit tests, at least for the `DatabaseCatalog`. Tests
    are written using `pytest` and a Github CI has been added.
    
    I'm going to close #2. We can open future tickets for specific unit tests that
    must be written.
    
    Closes #2

---
+++ tests/catalog/test_db.py
@@ -0,0 +1,128 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from datetime import datetime
+from recap.catalogs.db import DatabaseCatalog, CatalogEntry
+from recap.analyzers.db.location import Location
+from pathlib import Path
+
+class TestCatalogEntry:
+    def test_is_deleted(self):
+        entry = CatalogEntry(deleted_at=datetime.now())
+        assert entry.is_deleted() == True
+
+        entry = CatalogEntry(deleted_at=None)
+        assert entry.is_deleted() == False
+
+
+class TestDatabaseCatalog:
+
+    @pytest.fixture
+    def engine(self):
+        return create_engine('sqlite:///:memory:')
+
+    @pytest.fixture
+    def catalog(self, engine):
+        return DatabaseCatalog(engine)
+
+    def test_catalog_touch_doesnt_exist(self, catalog):
+        parent_path = Path("databases","postgresql", "instances", "localhost", "schemas", "some_db", "tables")
+        child_path = Path("some_table")
+
+        catalog.touch(parent_path / child_path)
+        assert catalog.ls(parent_path) == [str(child_path)]
+
+    def test_catalog_touch_does_exist(self, catalog):
+        parent_path = Path("databases","postgresql", "instances", "localhost", "schemas", "some_db", "tables")
+        child_path = Path("some_table")
+
+        catalog.touch(parent_path / child_path)
+        assert catalog.ls(parent_path) == [str(child_path)]
+
+        catalog.touch(parent_path / child_path)
+        assert catalog.ls(parent_path) == [str(child_path)]
+
+    def test_catalog_touch_deleted_path(self, catalog):
+        parent_path = Path("databases","postgresql", "instances", "localhost", "schemas", "some_db", "tables")
+        child_path = Path("some_table")
+
+        catalog.touch(parent_path / child_path)
+        assert catalog.ls(parent_path) == [str(child_path)]
+
+        catalog.rm(parent_path / child_path)
+        assert catalog.ls(parent_path) is None
+        assert catalog.read(parent_path / child_path) is None
+
+        catalog.touch(parent_path / child_path)
+        assert catalog.ls(parent_path) == [str(child_path)]
+
+    def test_write(self, catalog):
+        parent_path = Path("databases","postgresql", "instances", "localhost", "schemas", "some_db", "tables")
+        child_path = Path("some_table")
+
+        metadata = Location(database="postgresql",
+                            instance="localhost",
+                            schema="some_db",
+                            table="some_table")
+
+        catalog.write(parent_path / child_path, metadata.dict(), patch=False)
+        assert catalog.read(parent_path / child_path) == metadata.dict()
+
+    def test_write_metadata_after_rm(self, catalog):
+        parent_path = Path("databases","postgresql", "instances", "localhost", "schemas", "some_db", "tables")
+        child_path = Path("some_table")
+
+        metadata = Location(database="postgresql",
+                            instance="localhost",
+                            schema="some_db",
+                            table="some_table")
+
+        catalog.write(parent_path / child_path, metadata.dict(), patch=False)
+        assert catalog.read(parent_path / child_path) == metadata.dict()
+
+        catalog.rm(parent_path / child_path)
+        assert catalog.read(parent_path / child_path) is None
+
+    def test_rm(self, catalog):
+        parent_path = Path("databases","postgresql", "instances", "localhost", "schemas", "some_db", "tables")
+        child_path = Path("some_table")
+        catalog.touch(parent_path / child_path)
+        assert catalog.ls(parent_path) == sorted([str(child_path)])
+
+        catalog.rm(parent_path / child_path)
+        assert catalog.ls(parent_path / child_path) is None
+
+    def test_ls_no_entry(self, catalog):
+        parent_path = Path("databases","postgresql", "instances", "localhost", "schemas", "some_db", "tables")
+        assert catalog.ls(parent_path) is None
+
+    def test_ls_one_entry(self, catalog):
+        parent_path = Path("databases","postgresql", "instances", "localhost", "schemas", "some_db", "tables")
+        child_path = Path("table_one")
+        catalog.write(parent_path / child_path, {})
+        assert sorted(catalog.ls(parent_path)) == sorted([str(child_path)])
+
+    def test_ls_multiple_entries(self, catalog):
+        parent_path = Path("databases","postgresql", "instances", "localhost", "schemas", "some_db", "tables")
+        child_path_one = Path("table_one")
+        child_path_two = Path("table_two")
+        child_path_three = Path("table_three")
+        catalog.write(parent_path / child_path_one, {})
+        catalog.write(parent_path / child_path_two, {})
+        catalog.write(parent_path / child_path_three, {})
+        assert sorted(catalog.ls(parent_path)) == sorted([str(child_path_one), str(child_path_two), str(child_path_three)])
+
+    def test_search(self, catalog):
+        metadata = Location(database="db",
+                            instance="localhost",
+                            schema="some_db",
+                            table="some_table")
+        parent_path = Path("databases","postgresql", "instances", "localhost", "schemas", "some_db", "tables")
+        child_path = Path("some_table")
+
+        catalog.write(parent_path / child_path, metadata.dict(), patch=False)
+        search_result = catalog.search("json_extract(metadata, '$.\"database\"') = 'db'")
+
+        assert search_result == [metadata.dict()]
+
+
