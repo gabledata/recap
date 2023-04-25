05ad5e6a42fe626b8ab2412382387b8cc1832d59
Chris Riccomini <criccomini@apache.org>
2023-04-25 10:01:19 -0700

    Remove Avro and Proto from Python
    Since Recap's Java library does a better job with Avro and Proto, I've removed
    them from the Python library. I've also removed `diff` for the time being. I'm
    focusing the Python project on the Recap catalog and Recap crawler right now.
    
    I considered movign the crawler to Java as well, since KC has a SchemaMapper in
    its JDBC connector. The mapper is mostly for OLTP source DBs (MySQL, Postgres)
    so it's not quite as useful as I'd want (I'm focusing more on DWH for
    crawling).
    
    I also took the opportunity to clean things up a bit. I removed an unused BQ
    converter and cleaned some PDM dependencies.

--- python/recap/models.py
+++ python/recap/models.py
@@ -15,13 +15,3 @@ class Schema(BaseModel, extra=Extra.allow):
         # Parser throws exception if schema object is unparsable
         CONVERTER.to_recap_type(obj)
         return obj
-
-
-class Diff(BaseModel):
-    """
-    A difference for a Recap type attribute before and after it was changed.
-    """
-
-    path: str
-    before: Schema | None = None
-    after: Schema | None = None
