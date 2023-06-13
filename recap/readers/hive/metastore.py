871d372b6d765ca5a7e5d968e43e7e97c2c9c9b1
Kostas Pardalis <costas.pardalis@gmail.com>
2023-06-13 11:25:02 -0700

    update metastore.py
    added hive data types and basic hive metadata objects

--- recap/readers/hive/metastore.py
+++ recap/readers/hive/metastore.py
@@ -1,7 +1,9 @@
-from recap.readers.hive.thrift_gen import ThriftHiveMetastore as hms
-from recap.readers.hive.thrift_gen.ttypes import *
+from thrift_gen import ThriftHiveMetastore as hms
+from thrift_gen.ttypes import *
 from thrift.transport import TTransport, TSocket
 from thrift.protocol.TBinaryProtocol import TBinaryProtocol
+from enum import Enum
+from typing import Dict, List
 
 class HMS:
     def __init__(self, host='localhost', port=9090):
@@ -35,3 +37,169 @@ class HMS:
         
     def get_fields(self, database, table):
         return self.client.get_fields(database, table)
+    
+class PrincipalType(Enum):
+    ROLE = "ROLE"
+    USER = "USER"
+
+class PrimitiveCategory(Enum):
+    VOID = "VOID"
+    BOOLEAN = "BOOLEAN"
+    BYTE = "BYTE"
+    SHORT = "SHORT"
+    INT = "INT"
+    LONG = "LONG"
+    FLOAT = "FLOAT"
+    DOUBLE = "DOUBLE"
+    STRING = "STRING"
+    DATE = "DATE"
+    TIMESTAMP = "TIMESTAMP"
+    TIMESTAMPLOCALTZ = "TIMESTAMPLOCALTZ"
+    BINARY = "BINARY"
+    DECIMAL = "DECIMAL"
+    VARCHAR = "VARCHAR"
+    CHAR = "CHAR"
+    INTERVAL_YEAR_MONTH = "INTERVAL_YEAR_MONTH"
+    INTERVAL_DAY_TIME = "INTERVAL_DAY_TIME"
+    UNKNOWN = "UNKNOWN"
+
+class HTypeCategory(Enum):
+    PRIMITIVE = PrimitiveCategory
+    STRUCT = "STRUCT"
+    MAP = "MAP"
+    LIST = "LIST"
+    UNION = "UNION"
+
+class HType:
+    def __init__(self, name: str, category: HTypeCategory):
+        self.name = name
+        self.category = category
+    
+class HMapType(HType):
+    def __init__(self, keyType: HType, valueType: HType):
+        super().__init__("MAP", HTypeCategory.MAP)
+        self.keyType = keyType
+        self.valueType = valueType
+
+class HListType(HType):
+    def __init__(self, elementType: HType):
+        super().__init__("LIST", HTypeCategory.LIST)
+        self.elementType = elementType
+
+class HUnionType(HType):
+    def __init__(self, types: List[HType]):
+        super().__init__("UNION", HTypeCategory.UNION)
+        self.types = types
+
+class HVarcharType(HType):
+    def __init__(self, length: int):
+        MAX_VARCHAR_LENGTH = 65535
+        if length > MAX_VARCHAR_LENGTH:
+            raise ValueError("Varchar length cannot exceed 65535")
+        super().__init__("VARCHAR", HTypeCategory.PRIMITIVE)
+        self.length = length
+
+class HCharType(HType):
+    def __init__(self, length: int):
+        MAX_CHAR_LENGTH = 255
+        if length > MAX_CHAR_LENGTH:
+            raise ValueError("Char length cannot exceed 255")
+        super().__init__("CHAR", HTypeCategory.PRIMITIVE)
+        self.length = length
+
+class HDecimalType(HType):
+    def __init__(self, precision: int, scale: int):
+        MAX_PRECISION = 38
+        MAX_SCALE = 38
+        if precision > MAX_PRECISION:
+            raise ValueError("Decimal precision cannot exceed 38")
+        if scale > MAX_SCALE:
+            raise ValueError("Decimal scale cannot exceed 38")
+        super().__init__("DECIMAL", HTypeCategory.PRIMITIVE)
+        self.precision = precision
+        self.scale = scale
+
+class HStructType(HType):
+    def __init__(self, names: List[str], types:List[HType]):
+        if len(names) != len(types):
+            raise ValueError("mismatched size of names and types.")
+        if None in names:
+            raise ValueError("names cannot contain None")
+        if None in types:
+            raise ValueError("types cannot contain None")
+        
+        super().__init__("STRUCT", HTypeCategory.STRUCT)
+        self.names = names
+        self.types = types
+
+class HPrimitiveType(HType):
+    def __init__(self, primitiveType: PrimitiveCategory):
+        super().__init__(primitiveType.value, HTypeCategory.PRIMITIVE)
+        self.primitiveType = primitiveType
+
+class HDatabase:
+    def __init__(self, name: str, location: str =None, ownerName:str =None, ownerType: PrincipalType =None, comment=None, parameters: Dict[str, str]=None):
+        self.name = name
+        self.location = location
+        self.ownerName = ownerName
+        self.ownerType = ownerType
+        self.comment = comment
+        self.parameters = parameters
+    
+class HColumn:
+    def __init__(self, name: str, type: HType, comment: str =None):
+        self.name = name
+        self.type = type
+        self.comment = comment
+
+class StorageFormat:
+    def __init__(self, serde: str, inputFormat: str, outputFormat: str) -> None:
+        self.serde = serde
+        self.inputFormat = inputFormat
+        self.outputFormat = outputFormat
+
+#TODO: Implement
+class HiveBucketProperty:
+    def __init__(self):
+        # TODO!!
+        pass
+
+
+class HStorage:
+    def __init__(self, storageFormat: StorageFormat,
+                 skewed: bool = False, 
+                 location: str =None, 
+                 bucketProperty: HiveBucketProperty =None, 
+                 serdeParameters: Dict[str, str] =None):
+        
+        if storageFormat is None:
+            raise ValueError("storageFormat cannot be None")
+        self.storageFormat = storageFormat
+        self.skewed = skewed
+        self.location = location
+        self.bucketProperty = bucketProperty
+        self.serdeParameters = serdeParameters
+
+
+
+class HTable:
+    def __init__(self, databaseName: str, name: str, 
+                 tableType: str, columns: List[HColumn], 
+                 partitionColumns: List[HColumn],
+                 storage: HStorage, 
+                 parameters: Dict[str, str], 
+                 viewOriginalText: str = None, 
+                 viewExpandedText: str = None, 
+                 writeId: int = None, 
+                 owner: str = None):
+        
+        self.databaseName = databaseName
+        self.name = name
+        self.tableType = tableType
+        self.columns = columns
+        self.partitionColumns = partitionColumns
+        self.parameters = parameters
+        self.viewOriginalText = viewOriginalText
+        self.viewExpandedText = viewExpandedText
+        self.writeId = writeId
+        self.owner = owner
