4a74726913597ebca6c4075f912ed3f9e04d6c4d
Chris Riccomini <criccomini@apache.org>
2023-06-27 17:27:10 -0700

    Add JSONSchemaConverter support for ConfluentRegistryReader
    I've added "JSON" schema type support for the ConfluentRegistryReader. This is a
    basic implementation that doesn't support schema references.
    
    Closes #249

--- tests/unit/readers/test_confluent_registry.py
+++ tests/unit/readers/test_confluent_registry.py
@@ -78,3 +78,47 @@ def test_struct_protobuf():
     mock_schema_registry_client.get_latest_version.assert_called_with(
         "dummy_topic-value"
     )
+
+
+def test_struct_json_schema():
+    mock_schema_registry_client = MagicMock(spec=schema_registry.SchemaRegistryClient)
+
+    class MockSchema:
+        schema_type = "JSON"
+        schema_str = """
+        {
+          "$schema": "http://json-schema.org/draft-07/schema#",
+          "title": "User",
+          "type": "object",
+          "properties": {
+            "name": {
+              "type": "string"
+            },
+            "age": {
+              "type": "integer"
+            }
+          },
+          "required": ["name", "age"]
+        }
+        """
+
+    class MockRegisteredSchema:
+        schema = MockSchema()
+
+    mock_schema_registry_client.get_latest_version.return_value = MockRegisteredSchema()
+
+    reader = ConfluentRegistryReader(mock_schema_registry_client)
+    result = reader.struct("dummy_topic")
+
+    # Check that the schema was converted correctly.
+    assert isinstance(result, StructType)
+    assert len(result.fields) == 2
+    assert result.fields[0].extra_attrs["name"] == "name"
+    assert isinstance(result.fields[0], StringType)
+    assert result.fields[1].extra_attrs["name"] == "age"
+    assert isinstance(result.fields[1], IntType)
+
+    # Check that the get_latest_version method was called with the correct subject.
+    mock_schema_registry_client.get_latest_version.assert_called_with(
+        "dummy_topic-value"
+    )
