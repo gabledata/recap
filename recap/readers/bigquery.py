85f9a8ec1c344c0e5af4f6b7d6babdf0e3c92d6a
Chris Riccomini <criccomini@apache.org>
2023-07-18 10:30:50 -0700

    Add BigQueryReader.to_recap
    Recap can now convert BigQuery table schemas to Recap StructTypes.
    
    Some notes:
    
    1. Nested types are supported (RECORD, STRUCT)
    2. Repeated/ARRAYs are supported
    3. JSON types are treated as STRING
    4. STRING and BYTES have a max length of 65KiB by default
    
    For (4), I had a really hard time nailing down the exact max string size in BQ.
    [This](https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types)
    page says they have a 2 byte header on strings/bytes, which I assume is to store
    the byte length. Thus I went with 65KiB.
    
    I'm also using [bigquery-emulator](https://github.com/goccy/bigquery-emulator)
    to test. I've only included integration tests.
    
    Closes #285

---
+++ recap/readers/bigquery.py
@@ -0,0 +1,93 @@
+from google.cloud import bigquery
+
+from recap.types import (
+    BoolType,
+    BytesType,
+    FloatType,
+    IntType,
+    ListType,
+    StringType,
+    StructType,
+)
+
+
+class BigQueryReader:
+    def __init__(self, client: bigquery.Client):
+        self.client = client
+
+    def to_recap(self, dataset: str, table: str):
+        table_ref = self.client.dataset(dataset).table(table)
+        table_obj = self.client.get_table(table_ref)
+        return self._parse_fields(table_obj.schema)
+
+    def _parse_fields(self, fields: list[bigquery.SchemaField]) -> StructType:
+        recap_fields = []
+        for field in fields:
+            match field.field_type:
+                case "STRING" | "JSON":
+                    # I'm having a hard time finding the max string length in BQ. This link:
+                    # https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+                    # Says "2 logical bytes + the UTF-8 encoded string size", so I'm assuming
+                    # the 2 logical bytes are a uint16 length header, which is 65_536.
+                    field_type = StringType(bytes_=field.max_length or 65_536)
+                case "BYTES":
+                    field_type = BytesType(bytes_=field.max_length or 65_536)
+                case "INT64" | "INTEGER" | "INT" | "SMALLINT" | "TINYINT" | "BYTEINT":
+                    field_type = IntType(bits=64)
+                case "FLOAT" | "FLOAT64":
+                    field_type = FloatType(bits=64)
+                case "BOOLEAN":
+                    field_type = BoolType()
+                case "TIMESTAMP" | "DATETIME":
+                    field_type = IntType(
+                        logical="build.recap.Timestamp",
+                        bits=64,
+                        unit="microsecond",
+                    )
+                case "TIME":
+                    field_type = IntType(
+                        logical="build.recap.Time",
+                        bits=32,
+                        unit="microsecond",
+                    )
+                case "DATE":
+                    field_type = IntType(
+                        logical="build.recap.Date",
+                        bits=32,
+                        unit="day",
+                    )
+                case "RECORD" | "STRUCT":
+                    field_type = self._parse_fields(list(field.fields))
+                case "NUMERIC" | "DECIMAL":
+                    field_type = BytesType(
+                        logical="build.recap.Decimal",
+                        bytes_=16,
+                        variable=False,
+                        precision=field.precision or 38,
+                        scale=field.scale or 0,
+                    )
+                case "BIGNUMERIC" | "BIGDECIMAL":
+                    field_type = BytesType(
+                        logical="build.recap.Decimal",
+                        bytes_=32,
+                        variable=False,
+                        precision=field.precision or 76,
+                        scale=field.scale or 0,
+                    )
+                case _:
+                    raise ValueError(f"Unrecognized field type: {field.field_type}")
+
+            if field.mode == "REPEATED":
+                field_type = ListType(field_type)
+            if field.mode == "NULLABLE":
+                field_type = field_type.make_nullable()
+            if name := field.name:
+                field_type.extra_attrs["name"] = name
+            if description := field.description:
+                field_type.doc = description
+            if default := field.default_value_expression:
+                field_type.extra_attrs["default"] = default
+
+            recap_fields.append(field_type)
+
+        return StructType(recap_fields)
---
+++ tests/integration/readers/test_bigquery.py
@@ -0,0 +1,427 @@
+import pytest
+from google.api_core.client_options import ClientOptions
+from google.auth.credentials import AnonymousCredentials
+from google.cloud import bigquery
+from google.cloud.bigquery import QueryJobConfig
+
+from recap.readers.bigquery import BigQueryReader
+from recap.types import (
+    BoolType,
+    BytesType,
+    FloatType,
+    IntType,
+    ListType,
+    NullType,
+    StringType,
+    StructType,
+    UnionType,
+)
+
+
+@pytest.fixture(scope="module")
+def client():
+    client_options = ClientOptions(api_endpoint="http://localhost:9050")
+    client = bigquery.Client(
+        "test_project",
+        client_options=client_options,
+        credentials=AnonymousCredentials(),
+    )
+    return client
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup_data(client):
+    # Creating a table with all primitive types.
+    client.query(
+        query="""
+        CREATE TABLE `test_dataset.test_table` (
+            test_string STRING,
+            test_bytes BYTES,
+            test_int64 INT64,
+            test_float64 FLOAT64,
+            test_boolean BOOLEAN,
+            test_timestamp TIMESTAMP,
+            test_datetime DATETIME,
+            test_date DATE,
+            test_time TIME,
+            test_numeric NUMERIC,
+            test_bigdecimal BIGNUMERIC
+        )
+        """,
+        job_config=QueryJobConfig(),
+    )
+
+    # Creating a table with REQUIRED primitive types.
+    client.query(
+        query="""
+        CREATE TABLE `test_dataset.test_table_required` (
+            test_string STRING NOT NULL,
+            test_bytes BYTES NOT NULL,
+            test_int64 INT64 NOT NULL,
+            test_float64 FLOAT64 NOT NULL,
+            test_boolean BOOLEAN NOT NULL,
+            test_timestamp TIMESTAMP NOT NULL,
+            test_datetime DATETIME NOT NULL,
+            test_date DATE NOT NULL,
+            test_time TIME NOT NULL,
+            test_numeric NUMERIC NOT NULL,
+            test_bigdecimal BIGNUMERIC NOT NULL
+        )
+        """,
+        job_config=QueryJobConfig(),
+    )
+
+    # Creating a table with a nested STRUCT / RECORD type.
+    client.query(
+        query="""
+        CREATE TABLE `test_dataset.test_table_struct` (
+            test_record STRUCT<
+                test_boolean BOOLEAN,
+                nested_record STRUCT<
+                    test_nested_boolean BOOLEAN
+                >
+            >
+        )
+        """,
+        job_config=QueryJobConfig(),
+    )
+
+    # Creating a table with a repeated (array) type.
+    client.query(
+        query="""
+        CREATE TABLE `test_dataset.test_table_repeated` (
+            test_array ARRAY<STRING>
+        )
+        """,
+        job_config=QueryJobConfig(),
+    )
+
+    # Creating a table with a repeated RECORD type.
+    client.query(
+        query="""
+        CREATE TABLE `test_dataset.test_table_repeated_records` (
+            test_array ARRAY<STRUCT<test_boolean BOOLEAN, test_int64 INT64>>
+        )
+        """,
+        job_config=QueryJobConfig(),
+    )
+
+    # Creating a table with a default value.
+    client.query(
+        query="""
+        CREATE TABLE `test_dataset.test_table_default` (
+            test_string STRING DEFAULT "default_value"
+        )
+        """,
+        job_config=QueryJobConfig(),
+    )
+
+    # Creating a table with a column description.
+    client.query(
+        query="""
+        CREATE TABLE `test_dataset.test_table_description` (
+            test_string STRING OPTIONS(description="This is a test string column")
+        )
+        """,
+        job_config=QueryJobConfig(),
+    )
+
+
+def test_primitive_types(client):
+    reader = BigQueryReader(client)
+    recap_schema = reader.to_recap("test_dataset", "test_table")
+    recap_fields = recap_schema.fields
+
+    assert recap_fields[0] == UnionType(
+        types=[NullType(), StringType()],
+        default=None,
+        name="test_string",
+    )
+
+    assert recap_fields[1] == UnionType(
+        types=[NullType(), BytesType()],
+        default=None,
+        name="test_bytes",
+    )
+
+    assert recap_fields[2] == UnionType(
+        types=[NullType(), IntType(bits=64)],
+        default=None,
+        name="test_int64",
+    )
+
+    assert recap_fields[3] == UnionType(
+        types=[NullType(), FloatType(bits=64)],
+        default=None,
+        name="test_float64",
+    )
+
+    assert recap_fields[4] == UnionType(
+        types=[NullType(), BoolType()],
+        default=None,
+        name="test_boolean",
+    )
+
+    assert recap_fields[5] == UnionType(
+        types=[
+            NullType(),
+            IntType(
+                bits=64,
+                logical="build.recap.Timestamp",
+                unit="microsecond",
+            ),
+        ],
+        default=None,
+        name="test_timestamp",
+    )
+
+    assert recap_fields[6] == UnionType(
+        types=[
+            NullType(),
+            IntType(
+                bits=64,
+                logical="build.recap.Timestamp",
+                unit="microsecond",
+            ),
+        ],
+        default=None,
+        name="test_datetime",
+    )
+
+    assert recap_fields[7] == UnionType(
+        types=[
+            NullType(),
+            IntType(
+                bits=32,
+                logical="build.recap.Date",
+                unit="day",
+            ),
+        ],
+        default=None,
+        name="test_date",
+    )
+
+    assert recap_fields[8] == UnionType(
+        types=[
+            NullType(),
+            IntType(
+                bits=32,
+                logical="build.recap.Time",
+                unit="microsecond",
+            ),
+        ],
+        default=None,
+        name="test_time",
+    )
+
+    assert recap_fields[9] == UnionType(
+        types=[
+            NullType(),
+            BytesType(
+                bytes_=16,
+                variable=False,
+                logical="build.recap.Decimal",
+                precision=38,
+                scale=0,
+            ),
+        ],
+        default=None,
+        name="test_numeric",
+    )
+
+    assert recap_fields[10] == UnionType(
+        types=[
+            NullType(),
+            BytesType(
+                bytes_=32,
+                variable=False,
+                logical="build.recap.Decimal",
+                precision=76,
+                scale=0,
+            ),
+        ],
+        default=None,
+        name="test_bigdecimal",
+    )
+
+
+# TODO Remove xfail after https://github.com/goccy/bigquery-emulator/issues/210
+@pytest.mark.xfail(reason="BigQuery emulator does not support REQUIRED fields")
+def test_required_types(client):
+    reader = BigQueryReader(client)
+    recap_schema = reader.to_recap("test_dataset", "test_table_required")
+    recap_fields = recap_schema.fields
+
+    assert recap_fields[0] == StringType(name="test_string")
+
+    assert recap_fields[1] == BytesType(name="test_bytes")
+
+    assert recap_fields[2] == IntType(bits=64, name="test_int64")
+
+    assert recap_fields[3] == FloatType(bits=64, name="test_float64")
+
+    assert recap_fields[4] == BoolType(name="test_boolean")
+
+    assert recap_fields[5] == IntType(
+        bits=64,
+        logical="build.recap.Timestamp",
+        unit="microsecond",
+        name="test_timestamp",
+    )
+
+    assert recap_fields[6] == IntType(
+        bits=64,
+        logical="build.recap.Timestamp",
+        unit="microsecond",
+        name="test_datetime",
+    )
+
+    assert recap_fields[7] == IntType(
+        bits=32,
+        logical="build.recap.Date",
+        unit="day",
+        name="test_date",
+    )
+
+    assert recap_fields[8] == IntType(
+        bits=32,
+        logical="build.recap.Time",
+        unit="microsecond",
+        name="test_time",
+    )
+
+    assert recap_fields[9] == BytesType(
+        bytes_=16,
+        variable=False,
+        logical="build.recap.Decimal",
+        precision=38,
+        scale=0,
+        name="test_numeric",
+    )
+
+    assert recap_fields[10] == BytesType(
+        bytes_=32,
+        variable=False,
+        logical="build.recap.Decimal",
+        precision=76,
+        scale=0,
+        name="test_bigdecimal",
+    )
+
+
+def test_nested_struct_record_types(client):
+    reader = BigQueryReader(client)
+    recap_schema = reader.to_recap("test_dataset", "test_table_struct")
+    recap_fields = recap_schema.fields
+
+    assert recap_fields[0] == UnionType(
+        types=[
+            NullType(),
+            StructType(
+                fields=[
+                    UnionType(
+                        types=[
+                            NullType(),
+                            BoolType(),
+                        ],
+                        default=None,
+                        name="test_boolean",
+                    ),
+                    UnionType(
+                        types=[
+                            NullType(),
+                            StructType(
+                                fields=[
+                                    UnionType(
+                                        types=[
+                                            NullType(),
+                                            BoolType(),
+                                        ],
+                                        default=None,
+                                        name="test_nested_boolean",
+                                    ),
+                                ],
+                            ),
+                        ],
+                        default=None,
+                        name="nested_record",
+                    ),
+                ],
+            ),
+        ],
+        default=None,
+        name="test_record",
+    )
+
+
+def test_repeated_types(client):
+    reader = BigQueryReader(client)
+    recap_schema = reader.to_recap("test_dataset", "test_table_repeated")
+    recap_fields = recap_schema.fields
+
+    # ARRAYs can't be nullable and their elements can't be nullable. See:
+    # https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types#array_nulls
+
+    assert recap_fields[0] == ListType(
+        values=StringType(),
+        name="test_array",
+    )
+
+
+def test_repeated_records(client):
+    reader = BigQueryReader(client)
+    recap_schema = reader.to_recap("test_dataset", "test_table_repeated_records")
+    recap_fields = recap_schema.fields
+
+    assert recap_fields[0] == ListType(
+        values=StructType(
+            fields=[
+                UnionType(
+                    types=[
+                        NullType(),
+                        BoolType(),
+                    ],
+                    default=None,
+                    name="test_boolean",
+                ),
+                UnionType(
+                    types=[
+                        NullType(),
+                        IntType(bits=64),
+                    ],
+                    default=None,
+                    name="test_int64",
+                ),
+            ],
+        ),
+        name="test_array",
+    )
+
+
+# TODO Remove xfail after https://github.com/goccy/bigquery-emulator/issues/211
+@pytest.mark.xfail(reason="BigQuery emulator does not support DEFAULT values")
+def test_default_value(client):
+    reader = BigQueryReader(client)
+    recap_schema = reader.to_recap("test_dataset", "test_table_default")
+    recap_fields = recap_schema.fields
+
+    assert recap_fields[0] == UnionType(
+        types=[NullType(), StringType()],
+        default="default_value",
+        name="test_string",
+    )
+
+
+# TODO Remove xfail after https://github.com/goccy/bigquery-emulator/issues/212
+@pytest.mark.xfail(reason="BigQuery emulator does not support OPTIONS")
+def test_column_description(client):
+    reader = BigQueryReader(client)
+    recap_schema = reader.to_recap("test_dataset", "test_table_description")
+    recap_fields = recap_schema.fields
+
+    assert recap_fields[0] == UnionType(
+        types=[NullType(), StringType()],
+        default=None,
+        name="test_string",
+        docs="This is a test string column",
+    )
