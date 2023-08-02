0de4f0c489fc664414d2210ef750b3a5d2e2e1d8
Mike Perrone <mike@gable.ai>
2023-08-02 11:31:41 -0700

    update test harness to also define unimplemented invalid recap schemas

--- tests/unit/meta_schema/test_json_schema.py
+++ tests/unit/meta_schema/test_json_schema.py
@@ -16,11 +16,22 @@ def get_meta_schema() -> dict:
         return json.load(file)
 
 
-valid_schema_dir = "tests/spec"
+schema_dir = "tests/spec"
 valid_schema_paths = [
-    os.path.join(valid_schema_dir, file)
-    for file in os.listdir(valid_schema_dir)
-    if file.endswith(".json")
+    os.path.join(schema_dir, file)
+    for file in os.listdir(schema_dir)
+    if file.endswith(".json") and not file.endswith(".invalid.json")
+]
+invalid_schema_paths = [
+    os.path.join(schema_dir, file)
+    for file in os.listdir(schema_dir)
+    if file.endswith(".invalid.json")
+    and not file.endswith(".unimplemented.invalid.json")
+]
+unimplemented_invalid_schema_paths = [
+    os.path.join(schema_dir, file)
+    for file in os.listdir(schema_dir)
+    if file.endswith(".unimplemented.invalid.json")
 ]
 
 
@@ -30,3 +41,24 @@ def test_valid_schemas(schema_path):
     with open(schema_path, "r") as file:
         schema = json.load(file)
         validate(schema, meta_schema)  # throws ValidationError if invalid
+
+
+@pytest.mark.parametrize("schema_path", invalid_schema_paths)
+def test_invalid_schemas(schema_path):
+    meta_schema = get_meta_schema()
+    with open(schema_path, "r") as file:
+        schema = json.load(file)
+        with pytest.raises(ValidationError):
+            validate(schema, meta_schema)
+
+
+# If the JSON Schema were more accurate with the recap spec, we would expect these files to be invalid recap specs,
+# so validate() would throw. Mark these tests as expected to fail until implemented.
+@pytest.mark.xfail(strict=True)
+@pytest.mark.parametrize("schema_path", unimplemented_invalid_schema_paths)
+def test_unimplemented_invalid_schemas(schema_path):
+    meta_schema = get_meta_schema()
+    with open(schema_path, "r") as file:
+        schema = json.load(file)
+        with pytest.raises(ValidationError):
+            validate(schema, meta_schema)
