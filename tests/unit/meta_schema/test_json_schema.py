43ae1d31c52a0e424bd19fa26482110abc7e98cf
Mike Perrone <mike@gable.ai>
2023-08-01 17:03:42 -0700

    add test harness for meta schema correctness - with passing and failing test

---
+++ tests/unit/meta_schema/test_json_schema.py
@@ -0,0 +1,23 @@
+from curses import meta
+import pytest
+import json
+import os
+# import requests
+from jsonschema import validate, ValidationError
+
+def get_meta_schema() -> dict:
+    # response = requests.get("https://recap.build/specs/type/0.2.0.json")
+    # response.raise_for_status()
+    # return response.json()
+    with open("../recap-website/specs/type/0.2.0.json", 'r') as file:
+        return json.load(file)
+
+valid_schema_dir = "tests/spec"
+valid_schema_paths = [os.path.join(valid_schema_dir, file) for file in os.listdir(valid_schema_dir) if file.endswith('.json')]
+
+@pytest.mark.parametrize("schema_path", valid_schema_paths)
+def test_valid_schemas(schema_path):
+    meta_schema = get_meta_schema()
+    with open(schema_path, 'r') as file:
+        schema = json.load(file)
+        validate(schema, meta_schema)  # throws ValidationError if invalid
