eef6bb0931b7b178f0ccf977d9110dbdc9f16c83
Mike Perrone <mike@gable.ai>
2023-08-02 09:20:42 -0700

    pdm black

--- tests/unit/meta_schema/test_json_schema.py
+++ tests/unit/meta_schema/test_json_schema.py
@@ -1,23 +1,32 @@
-from curses import meta
-import pytest
 import json
 import os
+from curses import meta
+
+import pytest
+
 # import requests
-from jsonschema import validate, ValidationError
+from jsonschema import ValidationError, validate
+
 
 def get_meta_schema() -> dict:
     # response = requests.get("https://recap.build/specs/type/0.2.0.json")
     # response.raise_for_status()
     # return response.json()
-    with open("tests/0.2.0.json", 'r') as file:
+    with open("tests/0.2.0.json", "r") as file:
         return json.load(file)
 
+
 valid_schema_dir = "tests/spec"
-valid_schema_paths = [os.path.join(valid_schema_dir, file) for file in os.listdir(valid_schema_dir) if file.endswith('.json')]
+valid_schema_paths = [
+    os.path.join(valid_schema_dir, file)
+    for file in os.listdir(valid_schema_dir)
+    if file.endswith(".json")
+]
+
 
 @pytest.mark.parametrize("schema_path", valid_schema_paths)
 def test_valid_schemas(schema_path):
     meta_schema = get_meta_schema()
-    with open(schema_path, 'r') as file:
+    with open(schema_path, "r") as file:
         schema = json.load(file)
         validate(schema, meta_schema)  # throws ValidationError if invalid
