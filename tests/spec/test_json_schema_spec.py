864acea84cf54387d9b1da04cb2ccc83a55b6a86
Chris Riccomini <criccomini@users.noreply.github.com>
2023-08-08 16:02:28 -0700

    Update tests/spec/test_json_schema_spec.py
    Co-authored-by: Mike Perrone <mike.j.perrone@gmail.com>

--- tests/spec/test_json_schema_spec.py
+++ tests/spec/test_json_schema_spec.py
@@ -94,7 +94,7 @@ def test_invalid_schemas(filename: str, schema: dict, meta_schema: dict):
     "filename,schema",
     get_schemas(
         INVALID_SCHEMA_DIR,
-        lambda file: file.endswith(".json") and file.endswith(".unimplemented.json"),
+        lambda file: file.endswith(".unimplemented.json"),
     ),
 )
 def test_unimplemented_invalid_schemas(
