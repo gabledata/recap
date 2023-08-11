99fbc1a0fb878f094a4b8e8c9f749dfd157b5492
gwukelic <geoffrey@manifestdata.ai>
2023-08-10 17:23:20 -0700

    Add MySQL Reader
    Adding a MySQLReader.

---
+++ recap/readers/mysql.py
@@ -0,0 +1,83 @@
+from math import ceil
+from typing import Any
+
+from recap.readers.dbapi import DbapiReader
+from recap.types import BytesType, FloatType, IntType, RecapType, StringType
+
+
+class MysqlReader(DbapiReader):
+    def get_recap_type(self, column_props: dict[str, Any]) -> RecapType:
+        data_type = column_props["DATA_TYPE"].lower()
+        octet_length = column_props["CHARACTER_OCTET_LENGTH"]
+        precision = column_props["NUMERIC_PRECISION"]
+        scale = column_props["NUMERIC_SCALE"]
+
+        if data_type == "bigint":
+            # https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
+            base_type = IntType(bits=64, signed=True)
+        elif data_type in ["int", "integer"]:
+            # https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
+            base_type = IntType(bits=32, signed=True)
+        elif data_type == "mediumint":
+            # https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
+            base_type = IntType(bits=24, signed=True)
+        elif data_type == "smallint":
+            # https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
+            base_type = IntType(bits=16, signed=True)
+        elif data_type == "tinyint":
+            # https://dev.mysql.com/doc/refman/8.0/en/integer-types.html
+            base_type = IntType(bits=8, signed=True)
+        elif data_type == "double" or (data_type == "float" and precision > 23):
+            # https://dev.mysql.com/doc/refman/8.0/en/floating-point-types.html
+            base_type = FloatType(bits=64)
+        elif data_type == "float" and precision <= 23:
+            # https://dev.mysql.com/doc/refman/8.0/en/floating-point-types.html
+            base_type = FloatType(bits=32)
+        elif data_type in [
+            "text",
+            "json",
+            "mediumtext",
+            "longtext",
+            "tinytext",
+        ] or data_type.startswith("varchar"):
+            # This isn't exactly correct for JSON. MySQL uses a binary encoding
+            # to store the structure.
+            base_type = StringType(bytes_=octet_length, variable=True)
+        elif data_type.startswith("char") or data_type in ["enum", "set"]:
+            base_type = StringType(bytes_=octet_length, variable=False)
+        elif data_type in [
+            "blob",
+            "mediumblob",
+            "longblob",
+            "tinyblob",
+        ] or data_type.startswith("varbinary"):
+            base_type = BytesType(bytes_=octet_length, variable=True)
+        elif data_type.startswith("binary"):
+            base_type = BytesType(bytes_=octet_length, variable=False)
+        elif data_type.startswith("bit"):
+            base_type = BytesType(bytes_=8, variable=False)
+        elif data_type.startswith("timestamp") or data_type.startswith("datetime"):
+            dt_precision = column_props["DATETIME_PRECISION"]
+            unit = self._get_time_unit([dt_precision]) or "microsecond"
+            base_type = IntType(
+                bits=64,
+                logical="build.recap.Timestamp",
+                unit=unit,
+            )
+        elif data_type in ["dec", "decimal", "numeric"]:
+            base_type = BytesType(
+                logical="build.recap.Decimal",
+                bytes_=32,
+                variable=False,
+                precision=precision,
+                scale=scale,
+            )
+        elif data_type == "year":
+            base_type = IntType(
+                bits=16,
+                signed=False,
+            )  # Years are typically 2-byte values
+        else:
+            raise ValueError(f"Unknown data type: {data_type}")
+
+        return base_type
---
+++ tests/integration/readers/test_mysql.py
@@ -0,0 +1,237 @@
+import mysql.connector
+
+from recap.readers.mysql import MysqlReader
+from recap.types import (
+    BytesType,
+    FloatType,
+    IntType,
+    NullType,
+    StringType,
+    StructType,
+    UnionType,
+)
+
+
+class TestMySqlReader:
+    @classmethod
+    def setup_class(cls):
+        # Connect to the MySQL database
+        cls.connection = mysql.connector.connect(
+            database="testdb",
+            user="mysql",
+            password="password",
+            host="localhost",
+            port=3306,
+        )
+
+        # Create tables
+        cursor = cls.connection.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS test_types (
+                test_bigint BIGINT,
+                test_integer INTEGER,
+                test_smallint SMALLINT,
+                test_float FLOAT,
+                test_double DOUBLE PRECISION,
+                test_real REAL,
+                test_boolean BOOLEAN,
+                test_text TEXT,
+                test_char CHAR(10),
+                test_blob BLOB,
+                test_mediumblob MEDIUMBLOB,
+                test_longblob LONGBLOB,
+                test_mediumtext MEDIUMTEXT,
+                test_longtext LONGTEXT,
+                test_tinyblob TINYBLOB,
+                test_tinytext TINYTEXT,
+                test_varchar VARCHAR(255),
+                test_varbinary VARBINARY(255),
+                test_binary BINARY(255),
+                test_bit BIT(10),
+                test_timestamp TIMESTAMP,
+                test_decimal DECIMAL(10,2),
+                test_numeric NUMERIC(11,3),
+                test_not_null INTEGER NOT NULL,
+                test_not_null_default INTEGER NOT NULL DEFAULT 1,
+                test_default INTEGER DEFAULT 2
+            );
+        """
+        )
+        cls.connection.commit()
+
+    @classmethod
+    def teardown_class(cls):
+        # Delete tables
+        cursor = cls.connection.cursor()
+        cursor.execute("DROP TABLE IF EXISTS test_types;")
+        cls.connection.commit()
+
+        # Close the connection
+        cls.connection.close()
+
+    def test_struct_method(self):
+        # Initiate the MySQL Reader class
+        reader = MysqlReader(self.connection)  # type: ignore
+
+        # Test 'test_types' table. MySQL catalog is always 'def'.
+        test_types_struct = reader.to_recap("test_types", "testdb", "def")
+
+        # Define the expected output for 'test_types' table
+        expected_fields = [
+            UnionType(
+                default=None,
+                name="test_bigint",
+                types=[NullType(), IntType(bits=64, signed=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_integer",
+                types=[NullType(), IntType(bits=32, signed=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_smallint",
+                types=[NullType(), IntType(bits=16, signed=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_float",
+                types=[NullType(), FloatType(bits=32)],
+            ),
+            UnionType(
+                default=None,
+                name="test_double",
+                types=[NullType(), FloatType(bits=64)],
+            ),
+            UnionType(
+                default=None,
+                name="test_real",
+                types=[NullType(), FloatType(bits=64)],
+            ),
+            UnionType(
+                default=None,
+                name="test_boolean",
+                types=[NullType(), IntType(bits=8, signed=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_text",
+                types=[NullType(), StringType(bytes_=65_535, variable=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_char",
+                # 40 = max of 4 bytes in a UTF-8 encoded unicode character * 10 chars
+                types=[NullType(), StringType(bytes_=40, variable=False)],
+            ),
+            UnionType(
+                default=None,
+                name="test_blob",
+                types=[NullType(), BytesType(bytes_=65_535, variable=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_mediumblob",
+                types=[NullType(), BytesType(bytes_=16_777_215, variable=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_longblob",
+                types=[NullType(), BytesType(bytes_=4_294_967_295, variable=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_mediumtext",
+                types=[NullType(), StringType(bytes_=16_777_215, variable=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_longtext",
+                types=[NullType(), StringType(bytes_=4_294_967_295, variable=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_tinyblob",
+                types=[NullType(), BytesType(bytes_=255, variable=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_tinytext",
+                types=[NullType(), StringType(bytes_=255, variable=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_varchar",
+                types=[
+                    NullType(),
+                    StringType(bytes_=1020, variable=True),
+                ],  # max 4 bytes per char * 255
+            ),
+            UnionType(
+                default=None,
+                name="test_varbinary",
+                types=[NullType(), BytesType(bytes_=255, variable=True)],
+            ),
+            UnionType(
+                default=None,
+                name="test_binary",
+                types=[NullType(), BytesType(bytes_=255, variable=False)],
+            ),
+            UnionType(
+                default=None,
+                name="test_bit",
+                types=[NullType(), BytesType(bytes_=8, variable=False)],
+            ),
+            UnionType(
+                default=None,
+                name="test_timestamp",
+                types=[
+                    NullType(),
+                    IntType(
+                        bits=64, logical="build.recap.Timestamp", unit="microsecond"
+                    ),
+                ],
+            ),
+            UnionType(
+                default=None,
+                name="test_decimal",
+                types=[
+                    NullType(),
+                    BytesType(
+                        logical="build.recap.Decimal",
+                        bytes_=32,
+                        variable=False,
+                        precision=10,
+                        scale=2,
+                    ),
+                ],
+            ),
+            UnionType(
+                default=None,
+                name="test_numeric",
+                types=[
+                    NullType(),
+                    BytesType(
+                        logical="build.recap.Decimal",
+                        bytes_=32,
+                        variable=False,
+                        precision=11,
+                        scale=3,
+                    ),
+                ],
+            ),
+            IntType(bits=32, signed=True, name="test_not_null"),
+            IntType(bits=32, signed=True, name="test_not_null_default", default=b"1"),
+            UnionType(
+                default=b"2",
+                name="test_default",
+                types=[NullType(), IntType(bits=32, signed=True)],
+            ),
+        ]
+
+        # Going field by field to make debugging easier when test fails
+        for field, expected_field in zip(test_types_struct.fields, expected_fields):
+            assert field == expected_field
+
+        assert test_types_struct == StructType(fields=expected_fields)
